2e90baaf44b54abc4cbb2e96d1da72607567cf22
myfra <myfragithub@gmail.com>
2020-11-30 05:37:25 +0700

    string data type

---
+++ src/StringDataType.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tomy
+ */
+public class StringDataType {
+    public static void main(String[] args) {
+        
+        /**
+         * String Data Type
+         * - Must be using { "" }
+         * - Default Value is { null }
+         * - Combine string is using { + }
+         */
+        
+        String firstname = "Tomy";
+        String lastname = "Wibowo";
+        String fullname = firstname + " " + lastname;
+        
+        System.out.println(firstname);
+        System.out.println(lastname);
+        System.out.println(fullname);
+        
+    }
+}
