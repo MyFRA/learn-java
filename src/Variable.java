4b6a06d5bfdecdcfee423f45f4bf462dbdd6c711
myfra <myfragithub@gmail.com>
2020-11-30 11:06:53 +0700

    variable

---
+++ src/Variable.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tomy
+ */
+public class Variable {
+    public static void main(String[] args) {
+        
+        String name;
+        name = "MyFRA";
+        int age = 18;
+        String address = "Indonesia";
+        name = "Tomy Wibowo";
+
+        // Membuat Konstanta varible java
+        final String application = "Belajar Java";
+        
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+        System.out.println(application);
+        
+    }
+}
