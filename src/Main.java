9356a421cbdf8b92560146e850a9d9025bf81c54
MyFRA <tomyntapss@gmail.com>
2025-03-04 06:03:37 +0700

    introduction to Java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("I Like Pizza");
+        /**
+         * Introduction to Java
+         * 
+         */
+        System.out.println("I Like Pizza!");
+        System.out.println("It's really good!");
+        System.out.println("Buy me pizza!");
 
     }
 }
